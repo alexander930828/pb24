c1457a70fbceb85ab4efb0184ffb8e03ef0f13c5
donghun.lee <alexander930828>
2021-06-30 23:56:09 +0900

    confuse day

---
+++ print_all_friends.py
@@ -0,0 +1,31 @@
+def print_all_friends(g, start):
+    qu = []
+    done = set()
+
+    qu.append((start, 0))
+
+    done.add(start)
+
+    while qu:
+        (p, d) = qu.pop(0)
+        print(p, d)
+        for x in g[p]:
+            if x not in done:
+                qu.append((x, d+1))
+                done.add(x)
+
+
+fr_info = {
+    'summer': ['john', 'justin', 'mike'],
+    'john': ['summer', 'justin'],
+    'justin': ['john', 'summer', 'mike', 'may'],
+    'mike': ['summer', 'justin'],
+    'may': ['justin', 'kim'],
+    'kim': ['may'],
+    'tom': ['jerry'],
+    'jerry': ['tom']
+}
+
+print_all_friends(fr_info, 'summer')
+print()
+print_all_friends(fr_info, 'jerry')
